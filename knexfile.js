0c865ddd1d96677022cb58236ca3cbb9df153709
Viktor Dunaev <viktor.dunaev@routine-automation.com>
2021-10-18 17:58:21 +0300

    feat: set up environment variables

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,13 @@ module.exports = {
     development: {
         client: 'sqlite3',
         connection: {
-            filename: './data/dev.db3'
+            filename: './data/dev.db3',
         },
         pool:{
             afterCreate: (conn, done) => {
                 conn.run("PRAGMA foreign_keys = ON", done);
-            }
-        }
+            },
+        },
     },
     useNullAsDefault: true,
 };
