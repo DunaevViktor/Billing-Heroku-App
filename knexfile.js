badae6e9405041336fd20bb894e5547050dad6b1
Viktor Dunaev <viktor.dunaev@routine-automation.com>
2021-10-19 17:06:52 +0300

    refactor: added production to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,22 @@ module.exports = {
         connection: {
             filename: './data/dev.db3',
         },
-        pool:{
+        pool: {
             afterCreate: (conn, done) => {
                 conn.run("PRAGMA foreign_keys = ON", done);
             },
         },
     },
     production: {
-        
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tablename: 'knex_migrations',
+            directory: './migrations'
+        }
     }
 };
